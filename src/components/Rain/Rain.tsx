39cabb87c696728312184525ded26c8593c87a36
nodes777 <nodes777@gmail.com>
2024-04-17 09:25:48 +1000

    Batch updates snow

--- src/components/Rain/Rain.tsx
+++ src/components/Rain/Rain.tsx
@@ -8,14 +8,14 @@ const generateRainDrops = () => {
     const left = Math.random() * 2 * window.innerWidth;
     const top = Math.random() * window.innerHeight;
     // give each raindrop its own signal
-    const [drop, setDrop] = createSignal({
+    const drop = {
       top,
       left,
       // random rate between 1 and 5
       rate: Math.random() * 4 + 1,
-    });
+    };
 
-    rainDrops.push({ drop, setDrop });
+    rainDrops.push(drop);
   }
   return rainDrops;
 };
@@ -25,25 +25,26 @@ export const Rain = () => {
     document.body.style.overflow = "hidden"; // Hide scrollbar hack
   });
 
-  const rainDrops = generateRainDrops();
+  const [rainDrops, setRainDrops] = createSignal(generateRainDrops());
 
   const step = () => {
-    rainDrops.forEach((rainDrop) => {
-      const { top, left, rate } = rainDrop.drop();
+    const newDrops = rainDrops().map((rainDrop) => {
+      const { top, left, rate } = rainDrop;
       if (top > window.innerHeight || left < 0) {
-        rainDrop.setDrop({
+        return {
           top: 0,
           left: Math.random() * 2 * window.innerWidth,
           rate,
-        });
+        };
       } else {
-        rainDrop.setDrop({
+        return {
           top: top + rate,
           left: left - rate,
           rate,
-        });
+        };
       }
     });
+    setRainDrops(newDrops);
     requestAnimationFrame(step);
   };
 
@@ -51,12 +52,12 @@ export const Rain = () => {
 
   return (
     <div class={styles.rainContainer}>
-      {rainDrops.map((rainDrop, i) => (
+      {rainDrops().map((rainDrop, i) => (
         <div
           class={styles.rainDrop}
           style={{
-            left: `${rainDrop.drop().left}px`,
-            top: `${rainDrop.drop().top}px`,
+            left: `${rainDrop.left}px`,
+            top: `${rainDrop.top}px`,
           }}
         />
       ))}
