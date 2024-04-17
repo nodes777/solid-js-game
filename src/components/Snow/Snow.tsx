e4bb219c1ff359afe382f010beff3074577e5df6
nodes777 <nodes777@gmail.com>
2024-04-17 10:41:04 +1000

    Gets slow version working

--- src/components/Snow/Snow.tsx
+++ src/components/Snow/Snow.tsx
@@ -2,65 +2,66 @@ import { onMount, createSignal } from "solid-js";
 
 import styles from "./styles.module.css";
 
-const generatesnowFlakes = () => {
-  const snowFlakes = [];
-  for (let i = 0; i < 1000; i++) {
-    const left = Math.random() * 2 * window.innerWidth;
-    const top = Math.random() * window.innerHeight;
-    // give each snowFlake its own signal
-    const drop = {
-      top,
-      left,
-      // random rate between 1 and 5
-      rate: Math.random() * 4 + 1,
-    };
-
-    snowFlakes.push(drop);
-  }
-  return snowFlakes;
-};
-
 export const Snow = () => {
+  const generateSnowFlakes = () => {
+    const snowFlakes = [];
+    for (let i = 0; i < 1000; i++) {
+      const left = Math.random() * 2 * window.innerWidth;
+      const top = Math.random() * window.innerHeight;
+      // give each snowFlake its own signal
+      const [snowFlake, setSnowFlake] = createSignal({
+        top,
+        left,
+        // random rate between 1 and 5
+        rate: Math.random() * 4 + 1,
+      });
+
+      snowFlakes.push({ snowFlake, setSnowFlake });
+    }
+    return snowFlakes;
+  };
+
   onMount(() => {
     document.body.style.overflow = "hidden"; // Hide scrollbar hack
   });
 
-  const [snowFlakes, setSnowFlakes] = createSignal(generatesnowFlakes());
+  const snowFlakes = generateSnowFlakes();
 
   const step = () => {
-    const newDrops = snowFlakes().map((snowFlake) => {
-      const { top, left, rate } = snowFlake;
+    snowFlakes.forEach((snowFlake) => {
+      const { top, left, rate } = snowFlake.snowFlake();
       if (top > window.innerHeight || left < 0) {
-        return {
+        snowFlake.setSnowFlake({
           top: 0,
           left: Math.random() * 2 * window.innerWidth,
           rate,
-        };
+        });
       } else {
-        return {
+        snowFlake.setSnowFlake({
           top: top + rate,
           left: left - rate,
           rate,
-        };
+        });
       }
     });
-    setSnowFlakes(newDrops);
     requestAnimationFrame(step);
   };
 
   step();
 
   return (
-    <div class={styles.SnowContainer}>
-      {snowFlakes().map((snowFlake, i) => (
-        <div
-          class={styles.snowFlake}
-          style={{
-            left: `${snowFlake.left}px`,
-            top: `${snowFlake.top}px`,
-          }}
-        />
-      ))}
+    <div class={styles.snowContainer}>
+      {snowFlakes.map((snowFlake, i) => {
+        return (
+          <div
+            class={styles.snowFlake}
+            style={{
+              left: `${snowFlake.snowFlake().left}px`,
+              top: `${snowFlake.snowFlake().top}px`,
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
